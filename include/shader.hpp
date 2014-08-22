21da20a6f48fa68da2ed896c5880566c7105608e
Paul Hilbert <me@sicgraph.org>
2014-08-22 09:53:44 +0200

    shader class

--- include/shader.hpp
+++ include/shader.hpp
@@ -16,8 +16,10 @@
 
 #include <string>
 #include <memory>
+#include <map>
 
 #include "common.hpp"
+
 
 namespace harmont {
 
@@ -29,27 +31,42 @@ class shader {
 		typedef std::weak_ptr<shader<Stage>>          wptr;
 		typedef std::shared_ptr<const shader<Stage>>  const_ptr;
 		typedef std::weak_ptr<const shader<Stage>>    const_wptr;
+#ifdef USE_PLUSTACHE
+        typedef std::map<std::string, std::string>    parameters_t;
+#endif //USE_PLUSTACHE
 
 	public:
-		static ptr load(std::string filename);
+		static ptr from_file(std::string filename, bool compile_now = true);
+		static ptr from_source(std::string source, bool compile_now = true);
+#ifdef USE_PLUSTACHE
+		static ptr from_file(std::string filename, const parameters_t& params, bool compile_now = true);
+		static ptr from_source(std::string source, const parameters_t& params, bool compile_now = true);
+#endif // USE_PLUSTACHE
 		virtual ~shader();
 
-		GLenum stage();
-		GLuint ref();
+		constexpr GLenum stage() { return Stage; };
+
+		GLuint handle();
 
 	protected:
-		shader();
+		shader(std::string source, bool compile_now);
 
-		void read_code_(std::string filename);
+		void read_source_(std::string filename);
 		void create_();
 		void compile_();
 		void print_log_();
 
+        static std::string load_file_(const std::string& filename);
+
+#ifdef USE_PLUSTACHE
+        static std::string render_source_(const std::string& source, const parameters_t& params);
+#endif // USE_PLUSTACHE
+
 	protected:
-		GLuint  ref_;
-		GLchar* code_;
-		GLint   compile_status_;
-		GLint   link_status_;
+		GLuint      handle_;
+        std::string source_;
+		GLint       compile_status_;
+		GLint       link_status_;
 };
 
 
--- src/shader.cpp
+++ src/shader.cpp
@@ -15,82 +15,67 @@
 
 #include <iostream>
 #include <fstream>
-using std::ifstream;
+#include <streambuf>
 
 #include <cstdio>
 #include <cstdlib>
 
 #include <stdexcept>
 
+#ifdef USE_PLUSTACHE
+#include <plustache/plustache_types.hpp>
+#include <plustache/template.hpp>
+#endif // USE_PLUSTACHE
+
+
 namespace harmont {
 
 template <int Stage>
-typename shader<Stage>::ptr shader<Stage>::load(std::string filename) {
-	if (Stage != GL_VERTEX_SHADER && Stage != GL_FRAGMENT_SHADER && Stage != GL_GEOMETRY_SHADER) {
-		return ptr();
-	}
-	ptr result(new shader());
-
-	result->read_code_(filename);
-	result->create_();
-	result->compile_();
-
-	return result;
+typename shader<Stage>::ptr shader<Stage>::from_file(std::string filename, bool compile_now) {
+    return from_source(load_file_(filename, compile_now));
 }
+
+template <int Stage>
+typename shader<Stage>::ptr shader<Stage>::from_source(std::string source, bool compile_now) {
+    return std::make_shared<shader<Stage>>(source, compile_now);
+}
+
+#ifdef USE_PLUSTACHE
+template <int Stage>
+typename shader<Stage>::ptr shader<Stage>::from_file(std::string filename, const parameters_t& params, bool compile_now) {
+    return from_source(load_file_(filename), params, compile_now);
+}
+
+template <int Stage>
+typename shader<Stage>::ptr shader<Stage>::from_source(std::string source, const parameters_t& params, bool compile_now) {
+    return from_source(render_source_(source, params), compile_now);
+}
+#endif // USE_PLUSTACHE
 
 template <int Stage>
 shader<Stage>::~shader() {
-	if (code_) delete [] code_;
 }
 
 template <int Stage>
-inline GLenum shader<Stage>::stage() {
-	return Stage;
-}
-template <int Stage>
-inline GLuint shader<Stage>::ref() {
-	return ref_;
+inline GLuint shader<Stage>::handle() {
+	return handle_;
 }
 
 template <int Stage>
-shader<Stage>::shader() : ref_(0), code_(NULL), compile_status_(0), link_status_(0) {
-}
-
-template <int Stage>
-void shader<Stage>::read_code_(std::string filename) {
-	std::ifstream f;
-	f.open(filename.c_str(), std::ios::binary);
-	if(!f.is_open() || !f.good()) {
-		throw std::runtime_error("shader::read_code_: Could not open file for reading."+SPOT);
-		return;
-	}
-	std::ifstream::pos_type begin_pos = f.tellg();
-	f.seekg(0, std::ios_base::end);
-	int vertFileSize = static_cast<int>(f.tellg() - begin_pos);
-	f.seekg(0, std::ios_base::beg);
-	code_ = new GLchar[vertFileSize + 1];
-	code_[vertFileSize] = 0;
-	f.read(code_, vertFileSize);
-	if (!code_) {
-		throw std::runtime_error("shader::read_code_: No shader code read."+SPOT);
-	}
-}
-
-template <int Stage>
-void shader<Stage>::create_() {
-	ref_ = glCreateShader(Stage);
-	const GLchar* code = code_;
-	glShaderSource(ref_, 1, &code, NULL);
+shader<Stage>::shader(std::string source, bool compile_now) : handle_(0), source_(source), compile_status_(0), link_status_(0) {
+    if (compile_now) compile_();
 }
 
 template <int Stage>
 void shader<Stage>::compile_() {
-	glCompileShader(ref_);
-	glGetShaderiv(ref_, GL_COMPILE_STATUS, &compile_status_);
+	handle_ = glCreateShader(Stage);
+	glShaderSource(handle_, 1, source_.c_str(), NULL);
+	glCompileShader(handle_);
+	glGetShaderiv(handle_, GL_COMPILE_STATUS, &compile_status_);
 	print_log_();
 
 	if (!compile_status_) {
-		throw std::runtime_error("shader::compile_: Could not open compile shader."+SPOT);
+		throw std::runtime_error("shader::compile_: Could not compile shader."+SPOT);
 	}
 }
 
@@ -100,14 +85,14 @@ void shader<Stage>::print_log_() {
 	int charsWritten  = 0;
 	GLchar *infoLog;
 
-	glGetShaderiv(ref_, GL_INFO_LOG_LENGTH, &infologLength);
+	glGetShaderiv(handle_, GL_INFO_LOG_LENGTH, &infologLength);
 
 	if (infologLength > 1) {
 		infoLog = (GLchar *)malloc(infologLength);
 		if (infoLog == NULL) {
 			throw std::runtime_error("shader::print_log_: Could not allocate info log buffer."+SPOT);
 		}
-		glGetShaderInfoLog(ref_, infologLength, &charsWritten, infoLog);
+		glGetShaderInfoLog(handle_, infologLength, &charsWritten, infoLog);
 		switch (Stage) {
 			case GL_VERTEX_SHADER:   std::cout << "Vertex Shader InfoLog:\n"+std::string(infoLog) << std::endl;   break;
 			case GL_FRAGMENT_SHADER: std::cout << "Fragment Shader InfoLog:\n"+std::string(infoLog) << std::endl; break;
@@ -118,7 +103,18 @@ void shader<Stage>::print_log_() {
 	}
 }
 
+template <int Stage>
+std::string shader<Stage>::load_file_(const std::string& filename) {
+    std::ifstream in(filename.c_str());
+    if (!in.good()) {
+        throw std::runtime_error("shader::load_file_: Unable to open file \"" + filename + "\" for reading" + SPOT);
+    }
+    return str((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>());
+}
+
+
 } // harmont
+
 
 template class harmont::shader<GL_VERTEX_SHADER>;
 template class harmont::shader<GL_FRAGMENT_SHADER>;
